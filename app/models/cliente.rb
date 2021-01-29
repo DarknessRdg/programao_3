85f5b51e2873db1616110c29bca0df641bc7488f
Luan Rodrigues <luanrodrigues007@hotmail.com>
2021-01-29 11:18:17 -0300

    adicionar validcoes do cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,6 @@
 class Cliente < ApplicationRecord
   attr_accessible :nome, :idade
+
+  validates_presence_of :nome, message: " - deve ser preenchido"
+  validates_uniqueness_of :nome, message: " - nome já cadastrado"
 end
