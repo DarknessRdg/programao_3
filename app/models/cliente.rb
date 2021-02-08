4ecad32dd08519d1d87cc51d36c94eba2e920c1e
Luan Rodrigues <luanrodrigues007@hotmail.com>
2021-02-08 03:22:58 -0300

    COMPLETANDO O NOSSO DOMINIO

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,9 +1,2 @@
 class Cliente < ApplicationRecord
-  has_many :qualificacoes
-
-  attr_accessible :nome, :idade
-
-  validates_presence_of :nome, message: " - deve ser preenchido"
-  validates_uniqueness_of :nome, message: " - nome já cadastrado"
-  validates_numericality_of :idade, greater_than: 0, less_than: 100, message: " - deve ser um número entre 0 e 100"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :qualificacoes
+  resources :clientes
   resources :pessoas
   resources :restaurantes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
