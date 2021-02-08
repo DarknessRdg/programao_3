0c604755c7f64d138e16c60617c609b3a7e6a974
Luan Rodrigues <luanrodrigues007@hotmail.com>
2021-02-08 04:12:41 -0300

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_29_151140) do
+ActiveRecord::Schema.define(version: 2021_02_08_070724) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "nome", limit: 80
     t.integer "idade"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comentarios", force: :cascade do |t|
+    t.text "conteudo"
+    t.integer "comentavel_id"
+    t.string "comentavel_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["comentavel_id"], name: "index_comentarios_on_comentavel_id"
+    t.index ["comentavel_type"], name: "index_comentarios_on_comentavel_type"
   end
 
   create_table "pessoas", force: :cascade do |t|
