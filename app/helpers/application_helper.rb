6160d6fb5b3092f2dd764d5b30ffe27b6afe1a52
Luan Rodrigues <luanrodrigues007@hotmail.com>
2021-02-08 04:02:57 -0300

    LISTANDO OS COMENTARIOS DE UM COMENTAVEL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,8 @@ module ApplicationHelper
   def novo_comentario(comentavel)
     render partial: "comentarios/novo_comentario", locals: {comentavel: comentavel}
   end
+
+  def comentarios(comentavel)
+    render partial: "comentarios/comentarios", locals: {comentarios: comentavel.comentarios}
+  end
 end
